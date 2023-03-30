3af87d8216c3d653ccf48116183d2e3233102e32
Mounia El koraichi <mel-kora@e2r12p9.1337.ma>
2023-03-30 17:58:52 +0000

    getting started

--- lib.hpp
+++ lib.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-kora <mel-kora@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:13:32 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/03/30 14:20:11 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/03/30 16:20:11 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 # include <iostream>
 # include <fstream>
 # include <cstring>
+# include <cstdlib>
+# include <cstdio>
+# include <fcntl.h>
+# include <unistd.h>
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,18 +6,28 @@
 /*   By: mel-kora <mel-kora@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:13:21 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/03/30 14:23:23 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/03/30 17:57:52 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib.hpp"
 
+void ft_exit(std::string msg)
+{
+	std::cerr << "\033[0;91mError: " + msg + "\n\033[0m";
+	exit(1);
+}
+
 int main(int ac, char **av)
 {
-	if (ac == 2)
-	{
-		(void) av;
-	}
-	else
-		std::cout << "\033[0;91mError: Either too little or too much arguments 👽\n\033[0m";
+	std::string		content;
+	
+	if (ac != 2)
+		ft_exit("Either too little or too much arguments 👽");
+	std::ifstream conf_file(av[1]);
+	if (!conf_file.is_open())
+		ft_exit("Cannot open configuration file 👽");
+	std::getline(conf_file, content, '\0');
+	//db arak lkhdma ><
+	return 0;
 }
