2f059c5d7a4c143049cc1757e154aadec9ed91af
Mounia El koraichi <mel-kora@e2r12p9.1337.ma>
2023-03-30 18:23:11 +0000

    getting started

--- lib.hpp
+++ lib.hpp
@@ -6,7 +6,7 @@
 /*   By: mel-kora <mel-kora@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 14:13:32 by mel-kora          #+#    #+#             */
-/*   Updated: 2023/03/30 18:10:08 by mel-kora         ###   ########.fr       */
+/*   Updated: 2023/03/30 18:23:06 by mel-kora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,5 @@
 # include <fcntl.h>
 # include <unistd.h>
 
-
-void    ft_exit(std::string msg);
+void	ft_exit(std::string msg);
 #endif
