c9cfefa4d7ebf9bd310b8e1eaa7c4b55be6657b5
Mounia El koraichi <mel-kora@e2r11p2.1337.ma>
2023-04-08 16:45:55 +0000

    getting started

---
+++ Parsing/Configuration.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Configuration.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-kora <mel-kora@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/31 16:16:09 by mel-kora          #+#    #+#             */
+/*   Updated: 2023/03/31 16:17:20 by mel-kora         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONFIGURATION_HPP
+# define CONFIGURATION_HPP
+# include "../lib.hpp"
+
+class Configuration
+{
+	public:
+		Configuration(std::string content);
+};
+
+#endif
